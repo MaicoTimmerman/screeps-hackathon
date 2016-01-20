c316a9f83ef8621123bad9e98c909fd3bd0370e2
Maico Timmerman <maico.timmerman@gmail.com>
2016-01-20 16:30:27 +0100

    Update

--- dist/spawn_controller.js
+++ dist/spawn_controller.js
@@ -83,37 +83,19 @@ module.exports = {
         var creep_parts = require('creep_' + creep_type).get_parts();
         var creep_name = this.gen_creep_name(creep_type);
 
-        var spawns = _.filter(Game.spawns, {owner: 'MaicoTimmerman'});
-        console.log(spawns);
-        if(!spawns.length) return;
-
-
-
-
-        var available_spawn_points = spawns.filter(function(spawn) {
-            // TODO: canCreateCreep()
-            return spawn.canCreateCreep(creep_parts, creep_name);
+        var spawn_points = _.filter(Game.spawns, function(spawn) {
+            return spawn.canCreateCreep(creep_parts) == OK;
         });
-        if(!available_spawn_points.length) return;
 
-        spawn_point = available_spawn_points[0];
-        this._spawn_creep(creep_type, spawn_point);
+        if(!spawn_points.length) return;
 
-        Memory.spawnQue.pop();
+        var spawn_point = spawn_points[0];
+        this._spawn_creep(creep_type, creep_parts, creep_name, spawn_point);
     },
-    gen_creep_name: function(creep_type) {
-        var name_count = 0;
-        var name = null;
-        while(true) {
-            name = creep_type + nameCount;
-            if(Game.creeps[name] == undefined) break;
-            name_count++;
-        }
-        return name;
-    },
-    _spawn_creep: function(creep_type, creep_parts, spawnPoint) {
+    _spawn_creep: function(creep_type, creep_parts, creep_name, spawnPoint) {
         if(!spawnPoint) spawnPoint = Game.spawns.Spawn1;
-        console.log('Spawning ' + role);
+        console.log('Spawning ' + creep_type + ' (' + creep_parts + ')');
         spawnPoint.createCreep(creep_parts, creep_name, {role: creep_type});
+        Memory.spawn_queue.shift();
     },
 }
